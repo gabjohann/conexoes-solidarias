5df2deae347c4133c0be1bb325215c69120996ee
Lucas Johann <gabif.johann@gmail.com>
2024-09-15 18:20:29 -0300

    add: home structure

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,27 @@
+import { Footer } from './components/footer'
+import { Header } from './components/header'
+
 export function App() {
-  return <h1>Conexões Solidárias</h1>
+  return (
+    <div className="flex flex-col h-screen w-screen px-6">
+      <Header />
+      <div className="flex flex-1 flex-col gap-10 text-white font-light justify-center max-w-[700px] pl-24">
+        <h1 className="font-semibold text-2xl">Conexões Solidárias</h1>
+        <p className="leading-8">
+          Uma iniciativa do{' '}
+          <span className="font-semibold text-laranjaSenac">
+            Conexão Senac Canoas
+          </span>
+          , que busca unir pessoas afetadas pela enchente que atingiu o{' '}
+          <span className="font-semibold text-laranjaSenac">
+            Rio Grande do Sul
+          </span>{' '}
+          com empresas, ONGs, entidades médicas e outros setores. <br /> O
+          objetivo é fornecer apoio na reestruturação das suas vidas pessoais,
+          profissionais e psicológicas.
+        </p>
+      </div>
+      <Footer />
+    </div>
+  )
 }
