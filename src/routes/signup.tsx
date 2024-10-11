e0ec6f043332dbf6e0061ba5861441be438d7b87
Lucas Gabriel <gabif.johann@gmail.com>
2024-10-10 22:16:09 -0300

    refactor: change form display

--- src/routes/signup.tsx
+++ src/routes/signup.tsx
@@ -37,97 +37,130 @@ export function SignUpForm() {
   }
 
   return (
-    <div className="flex flex-1 items-center justify-center">
+    <div className="flex items-center flex-1">
       <form
         onSubmit={handleSubmit(registerEnterprise)}
-        className="flex flex-col gap-4 max-w-2xl"
+        className="flex flex-col flex-1"
       >
-        <label htmlFor="" className="text-white text-lg">
-          Dados pessoais
-        </label>
-        <div className="flex gap-3 items-center justify-between">
-          <Input
-            type="text"
-            placeholder="Nome"
-            className="flex-1"
-            {...register('name')}
-          />
-          <Input type="text" placeholder="Sobrenome" className="flex-1" />
-        </div>
+        <div className="flex items-start justify-around flex-1">
+          <div className="flex flex-col gap-4">
+            <label htmlFor="" className="text-lg text-white">
+              Dados pessoais
+            </label>
+            <div className="flex items-center justify-between gap-3">
+              <Input
+                type="text"
+                placeholder="Nome"
+                className="flex-1"
+                {...register('name')}
+              />
+              <Input type="text" placeholder="Sobrenome" className="flex-1" />
+            </div>
 
-        <div className="flex items-center gap-3">
-          <Input type="number" placeholder="CPF" className="flex-1" />
-          <Input type="number" placeholder="Celular" className="flex-1" />
-        </div>
-        <div className="flex items-center gap-3">
-          <Input
-            type="number"
-            placeholder="Digite sua senha"
-            className="flex-1"
-          />
-          <Input
-            type="number"
-            placeholder="Confirme a senha"
-            className="flex-1"
-          />
-        </div>
-        <Input
-          type="email"
-          placeholder="Digite seu e-mail"
-          className="flex-1"
-        />
+            <div className="flex items-center gap-3">
+              <Input type="number" placeholder="CPF" className="flex-1" />
+              <Input type="number" placeholder="Celular" className="flex-1" />
+            </div>
+            <div className="flex items-center gap-3">
+              <Input
+                type="number"
+                placeholder="Digite sua senha"
+                className="flex-1"
+              />
+              <Input
+                type="number"
+                placeholder="Confirme a senha"
+                className="flex-1"
+              />
+            </div>
+            <Input
+              type="email"
+              placeholder="Digite seu e-mail"
+              className="flex-1"
+            />
+            <div className="flex items-end gap-3">
+              <label htmlFor="typeOfHelp" className="text-white">
+                Informe seu cargo na empresa:
+              </label>
+              <Select>
+                <SelectTrigger className="flex-1 border-laranjaSenac text-white/70">
+                  <SelectValue placeholder="Cargo" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="ceo">Dono</SelectItem>
+                  <SelectItem value="cto">Sócio</SelectItem>
+                  <SelectItem value="hr">RH</SelectItem>
+                  <SelectItem value="other">Outro</SelectItem>
+                </SelectContent>
+              </Select>
+            </div>
+          </div>
 
-        <label htmlFor="" className="text-white text-lg pt-6">
-          Dados da empresa
-        </label>
-        <div className="flex items-center justify-between">
-          <Select>
-            <SelectTrigger className="flex-1 mr-3 border-laranjaSenac text-white/70">
-              <SelectValue placeholder="Serviço prestado" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectItem value="discount">Desconto</SelectItem>
-              <SelectItem value="donation">Doação</SelectItem>
-              <SelectItem value="volunteering">Voluntariado</SelectItem>
-              <SelectItem value="psychologicalCare">
-                Atendimento Psicológico
-              </SelectItem>
-            </SelectContent>
-          </Select>
+          <div className="flex flex-col gap-4">
+            <label htmlFor="" className="text-lg text-white">
+              Dados da empresa
+            </label>
+            <div className="flex items-center justify-between">
+              <div className="flex items-end gap-3">
+                <Input type="number" placeholder="CEP" className="max-w-56" />
+                <button
+                  className="underline text-white/70 hover:text-white"
+                  type="button"
+                >
+                  Não sei meu CEP
+                </button>
+              </div>
+            </div>
 
-          <div className="flex gap-3 items-end">
-            <Input type="number" placeholder="CEP" className="max-w-56" />
-            <button
-              className="text-white/70 hover:text-white underline"
-              type="button"
-            >
-              Não sei meu CEP
-            </button>
+            <div className="flex gap-3">
+              <Input type="text" placeholder="Endereço" className="flex-1" />
+              <Input type="number" placeholder="Número" className="max-w-28" />
+            </div>
+
+            <div className="flex gap-3">
+              <Input type="text" placeholder="Bairro" className="max-w-60" />
+              <Input type="text" placeholder="Cidade" />
+              <Input type="text" placeholder="Estado" className="max-w-28" />
+            </div>
+
+            <div className="flex items-center gap-3">
+              <Input type="number" placeholder="CNPJ" className="flex-1" />
+              <Input
+                type="text"
+                placeholder="Razão social"
+                className="flex-1"
+              />
+            </div>
+
+            <div className="flex items-end gap-3">
+              <label htmlFor="typeOfHelp" className="text-white">
+                Informe o serviço prestado:
+              </label>
+              <Select>
+                <SelectTrigger className="flex-1 border-laranjaSenac text-white/70">
+                  <SelectValue placeholder="Serviço prestado" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="discount">Desconto</SelectItem>
+                  <SelectItem value="donation">Doação</SelectItem>
+                  <SelectItem value="volunteering">Voluntariado</SelectItem>
+                  <SelectItem value="psychologicalCare">
+                    Atendimento Psicológico
+                  </SelectItem>
+                </SelectContent>
+              </Select>
+            </div>
           </div>
         </div>
 
-        <div className="flex gap-3">
-          <Input type="text" placeholder="Endereço" className="flex-1" />
-          <Input type="number" placeholder="Número" className="max-w-28" />
+        <div className="flex flex-col items-center justify-center">
+          <button
+            type="submit"
+            className="px-56 py-3 font-semibold rounded-full mt-14 bg-laranjaSenac text-azulSenac hover:font-bold hover:bg-laranjaSenac/70"
+          >
+            Enviar
+          </button>
         </div>
-
-        <div className="flex gap-3">
-          <Input type="text" placeholder="Bairro" className="max-w-60" />
-          <Input type="text" placeholder="Cidade" />
-          <Input type="text" placeholder="Estado" className="max-w-28" />
-        </div>
-
-        <div className="flex items-center gap-3">
-          <Input type="number" placeholder="CNPJ" className="flex-1" />
-          <Input type="text" placeholder="Razão social" className="flex-1" />
-        </div>
-
-        <button
-          type="submit"
-          className="bg-laranjaSenac rounded-full py-3 mt-2 text-azulSenac font-semibold hover:font-bold hover:bg-laranjaSenac/70"
-        >
-          Enviar
-        </button>
       </form>
     </div>
   )
