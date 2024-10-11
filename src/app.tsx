ad9d361e3bdfbc544ca83abc6b8a514d53dc1d4c
Lucas Johann <gabif.johann@gmail.com>
2024-10-10 21:33:19 -0300

    add: routes

--- src/app.tsx
+++ src/app.tsx
@@ -1,27 +1,18 @@
+import { Outlet, useLocation } from 'react-router-dom'
 import { Footer } from './components/footer'
 import { Header } from './components/header'
+import { MinFooter } from './components/minFooter'
 
 export function App() {
+  const location = useLocation()
+
   return (
     <div className="flex flex-col h-screen w-screen px-6">
       <Header />
-      <div className="flex flex-1 flex-col gap-10 text-white font-light justify-center max-w-[700px] pl-24">
-        <h1 className="font-semibold text-2xl">Conexões Solidárias</h1>
-        <p className="leading-8">
-          Uma iniciativa do{' '}
-          <span className="font-semibold text-laranjaSenac">
-            Conexão Senac Canoas
-          </span>
-          , que busca unir pessoas afetadas pela enchente que atingiu o{' '}
-          <span className="font-semibold text-laranjaSenac">
-            Rio Grande do Sul
-          </span>{' '}
-          com empresas, ONGs, entidades médicas e outros setores. <br /> O
-          objetivo é fornecer apoio na reestruturação das suas vidas pessoais,
-          profissionais e psicológicas.
-        </p>
-      </div>
-      <Footer />
+
+      <Outlet />
+
+      {location.pathname === '/signup' ? <MinFooter /> : <Footer />}
     </div>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,35 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { App } from './app'
+import { Home } from './routes/home'
+import { SignUpForm } from './routes/signup'
 import './index.css'
+import { LoginForm } from './routes/login'
+
+const routes = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: 'signup',
+        element: <SignUpForm />,
+      },
+      {
+        path: 'login',
+        element: <LoginForm />,
+      },
+    ],
+  },
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={routes} />
   </StrictMode>
 )
